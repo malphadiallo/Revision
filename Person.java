7f39c220278c1460affd4531deef093674a8ef30
malphadiallo <47794587+malphadiallo@users.noreply.github.com>
2020-04-06 03:16:33 +0200

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,48 @@
+package main;
+
+public class Person {
+	private String  firstName;
+	private String lastName;
+	private int age;
+	public Person(String firstName, String lastName, int age) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean equals(Object obj) {
+		return (obj instanceof Person) && 
+				  ((Person)obj).getFirstName().equals(firstName) && 
+				  ((Person)obj).getLastName().equals(lastName);
+				  
+		
+	}
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
+	}
+	
+	
+
+	
+
+
+}
---
+++ Test.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Test {
+	
+	static Person p = new Person("Aissatou","diallo",3);
+    static Person t = new Person("Aissatou","diallo",3);
+    
+	public static void main(String[] args) {
+		System.out.println("verif :" + (p==t));
+		
+		
+
+	}
+
+}
